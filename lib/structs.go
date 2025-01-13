b4781edeef3305908fd455e1bdd7e20934f54faf
mufaroxyz <mufaro.priv@gmail.com>
2025-01-13 01:05:34 +0100

    feat: add useful win api handles for later

--- lib/structs.go
+++ lib/structs.go
@@ -1,8 +1,43 @@
 package lib
 
+type (
+	HOOKPROC  func(int, WPARAM, LPARAM) LRESULT
+	HINSTANCE uintptr
+	HWND      uintptr
+	WPARAM    uintptr
+	LPARAM    uintptr
+	LRESULT   uintptr
+	DWORD     uint32
+	UINT      uint32
+	MSG       struct {
+		HWND    HWND
+		Message UINT
+		WParam  WPARAM
+		LParam  LPARAM
+		Time    DWORD
+		Pt      POINT
+	}
+)
+
 type POINT struct {
 	X, Y int32
 }
+
+const (
+	WH_MOUSE_LL    = 14
+	WM_LBUTTONDOWN = 0x0201
+	WM_RBUTTONDOWN = 0x0204
+)
+
+type MSLLHOOKSTRUCT struct {
+	Point     POINT
+	MouseData uint32
+	Flags     uint32
+	Time      uint32
+	ExtraInfo uintptr
+}
+
+type HHOOK uintptr
 
 type CursorPosData struct {
 	POINT
--- lib/winapi.go
+++ lib/winapi.go
@@ -6,13 +6,58 @@ import (
 )
 
 var (
-	WinApi           = syscall.NewLazyDLL("user32.dll")
-	procGetCursorPos = WinApi.NewProc("GetCursorPos")
+	WinApi                = syscall.NewLazyDLL("user32.dll")
+	procGetCursorPos      = WinApi.NewProc("GetCursorPos")
+	procSetWindowsHookExW = WinApi.NewProc("SetWindowsHookExW")
+	procUnhookWindowsHook = WinApi.NewProc("UnhookWindowsHook")
+	procLowLevelMouseProc = WinApi.NewProc("LowLevelMouseProc")
+	procCallNextHookEx    = WinApi.NewProc("CallNextHookEx")
+	procGetMessageW       = WinApi.NewProc("GetMessageW")
 )
 
 func GetProcAddress(name string) uintptr {
 	proc := WinApi.NewProc(name)
 	return proc.Addr()
+}
+
+func SetWindowsHookExW(idHook int, lpfn HOOKPROC, hMod HINSTANCE, dwThreadId DWORD) HHOOK {
+	ret, _, _ := procSetWindowsHookExW.Call(
+		uintptr(idHook),
+		uintptr(syscall.NewCallback(lpfn)),
+		uintptr(hMod),
+		uintptr(dwThreadId))
+	return HHOOK(ret)
+}
+
+func UnhookWindowsHook(hhk HHOOK) bool {
+	ret, _, _ := procUnhookWindowsHook.Call(uintptr(hhk))
+	return ret != 0
+}
+
+func LowLevelMouseProc(nCode int, wParam WPARAM, lParam LPARAM) LRESULT {
+	ret, _, _ := procLowLevelMouseProc.Call(
+		uintptr(nCode),
+		uintptr(wParam),
+		uintptr(lParam))
+	return LRESULT(ret)
+}
+
+func CallNextHookEx(hhk HHOOK, nCode int, wParam WPARAM, lParam LPARAM) LRESULT {
+	ret, _, _ := procCallNextHookEx.Call(
+		uintptr(hhk),
+		uintptr(nCode),
+		uintptr(wParam),
+		uintptr(lParam))
+	return LRESULT(ret)
+}
+
+func GetMessageW(lpMsg *MSG, hWnd HWND, wMsgFilterMin, wMsgFilterMax UINT) bool {
+	ret, _, _ := procGetMessageW.Call(
+		uintptr(unsafe.Pointer(lpMsg)),
+		uintptr(hWnd),
+		uintptr(wMsgFilterMin),
+		uintptr(wMsgFilterMax))
+	return ret != 0
 }
 
 func GetCursorPos(lpPoint *POINT) bool {
